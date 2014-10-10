f8126a415014a637e13b5e67c4cd9f11e898b574
Polcz Peter <ppolcz@gmail.com>
2014-10-10 21:33:55 +0200

    ELOZO COMMITHOZ: elfelejtettem az uj fajlokat hozzaadni, most megtortent
    Signed-off-by: Polcz Peter <ppolcz@localhost>

---
+++ CoreLibraryTest/src/com/napol/koltsegvetes/db/EColumnNamesTest.java
@@ -0,0 +1,105 @@
+package com.napol.koltsegvetes.db;
+
+import static com.napol.koltsegvetes.db.EColumnNames.*;
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EColumnNamesTest
+{
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void testSqlname()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSqltypeall()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSqltype()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSqlnameString()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testJavatype()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testTable()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRef()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsDateType()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToDate()
+    {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        String str = "2014-03-20";
+        Object obj = TR_DATE.toDate(str);
+        assertTrue(obj instanceof Date);
+        Date date = (Date) obj;
+        assertEquals(str, df.format(date));
+    }
+
+    @Test
+    public void testToString()
+    {
+        String str = "2014-03-20";
+        assertEquals(str, TR_DATE.toString(str));
+        assertEquals("2014-03-20", str);
+    }
+
+    @Test
+    public void testToQuoteString()
+    {
+        fail("Not yet implemented");
+    }
+
+}
